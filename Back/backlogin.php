31449ed6f72e0c6485f1650d0bdec1d9b18f09e4
Orazio Lentini <oraziio@hotmail.com>
2014-02-11 19:02:09 -0500

    Update backlogin.php

--- Back/backlogin.php
+++ Back/backlogin.php
@@ -5,20 +5,10 @@
     }
 
     mysql_select_db("ovl2_proj", $con);
-    
-    //$request_body = file_get_contents('php://input');
-    //$json = json_decode($request_body);
-    
-    //$User = $json->Username;
-    //$Pass = $json->Password; 
    
     $User = $_POST['Username'];
     $Pass = $_POST['Password'];
-    $H = $_POST['hp'];
-    
-    //creating new user
-    #$sql1 = "INSERT INTO Login(Username, Password) VALUES ('$Username', '$Password')";
-    #$exec = mysql_query($sql1,$con);
+    $Bounce = $_POST['check'];
     
     $sql = mysql_query("SELECT COUNT(*) FROM Login L WHERE Username = '$User' AND Password = '$Pass'");
     $info = mysql_fetch_assoc($sql);
@@ -26,29 +16,21 @@
     
     //echo $ver;
     
-    if($H == 302){
+    if($Bounce == 302){
         if($ver == 1) {
-            echo "User Found On NJIT Server and In Database.";
+            echo "User Found On NJIT Server and Database.";
         }
         else{
-            echo "User Found On NJIT Server But Not In Database."; 
+            echo "User Found On NJIT Server But Not On Database."; 
         }
     }
-    if($H != 302){
+    if($Bounce != 302){
         if($ver == 1) {
-            echo "User Not Found on NJIT Server But Found In Database.";
+            echo "User Not Found On NJIT Server But Found On Database.";
         }
         else{
-            echo "User Not Found on NJIT Server and In Database.";
+            echo "User Not Found On NJIT Server and On Database.";
         }
     }
-    /*
-    if($H == 302 && $ver == 0) {
-        echo "User Found On NJIT Server But Not In Database."; 
-        header('Location: home.html');
-    }
-    else{
-        echo "User Not Found on NJIT Server But Found In Database."
-        header('Location: index.html');
-    } */
+
 ?>
